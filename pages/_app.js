fcbba9d1f95a1bfae1b207b010578c02e0c8513b
eduschoof <eduardoschoof@gmail.com>
2024-08-29 17:19:11 -0300

    altera o index.js e add arquivos css, imagens pasta museu, _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/styles.css"; // Importe o CSS global aqui
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import styles from "../styles/Home.module.css";
+
 function Home() {
-  return <h1>Saudades da minha velhinha!</h1>;
+  return (
+    <div className={styles.container}>
+      <img
+        src="/images/under-construction.jpg"
+        alt="Um rato usando colete e bone amarelos, com o 
+        braço apoiado sobre uma pá e a cabeça apoiada sobre 
+        um cavalete onde está escrito under construction."
+        className={styles.image}
+      />
+      <h1>Colaborem</h1>
+      <h2>Um espaço onde todos podem aprender e ensinar.</h2>
+      <p>Está com dificuldades? Venha!</p>
+      <p>Quer ajudar? Venha!</p>
+    </div>
+  );
 }
 
 export default Home;
